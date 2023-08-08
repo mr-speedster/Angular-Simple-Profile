d4feeab74348fd101f6628e8bdf66a8581ffe327
Ajith D <ajithd78564@gmail.com>
2023-08-08 14:15:32 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AboutComponent } from './component/about/about.component';
+import { ResumeComponent } from './component/resume/resume.component';
+import { ServicesComponent } from './component/services/services.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent,
+    ResumeComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
